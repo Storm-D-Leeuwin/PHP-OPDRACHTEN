576ffccc78b0027e1851d4f4f4f0b1a2888a0c71
Storm Leeuwin <storm.leeuwin2@gmail.com>
2022-05-12 11:37:10 +0200

    Opd 2

---
+++ H02/H03/Opd02/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+    body {
+        text-align: center;
+    }
+</style>
+</head>
+<body>
+<?php
+for ($i = 0; $i <= 90; $i++) {
+    for ($j = 0; $j <= $i; $j++) {
+        echo "*";
+    }
+    echo "*<br>";
+}
+?>
+</body>
+</html>
