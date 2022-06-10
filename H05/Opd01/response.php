91665482c291cba998d6d5907793a311cd1cb5ee
Storm Leeuwin <storm.leeuwin2@gmail.com>
2022-06-10 18:33:41 +0200

    Opd 1

---
+++ H05/Opd01/response.php
@@ -0,0 +1,23 @@
+<?php
+
+if ($_POST['voornaam'] == "") {
+    echo "Je moet nog een naam invullen"."<br>";
+    echo "<a href=\"formulier.html\">Terug naar het formulier</a>"."<br>";
+};
+
+if ($_POST['adres'] == "") {
+    echo "Je moet nog een adres invullen"."<br>";
+    echo "<a href=\"formulier.html\">Terug naar het formulier</a>"."<br>";
+};
+
+if ($_POST['email'] == "") {
+    echo "Je moet nog een email invullen"."<br>";
+    echo "<a href=\"formulier.html\">Terug naar het formulier</a>"."<br>";
+};
+
+if ($_POST['wachtwoord'] == "") {
+    echo "Je moet nog een wachtwoord invullen"."<br>";
+    echo "<a href=\"formulier.html\">Terug naar het formulier</a>"."<br>";
+};
+
+
