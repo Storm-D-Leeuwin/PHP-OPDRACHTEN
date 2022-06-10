238956e90ca98cc699dc51ff6bab2437ef6283d5
Storm Leeuwin <storm.leeuwin2@gmail.com>
2022-06-10 19:51:03 +0200

    Opd 3

---
+++ H05/Opd03/response.php
@@ -0,0 +1,11 @@
+<?php
+
+// print_r($_POST);
+
+// echo "<img src='apen/".$_POST['apen'].".jpg'>";
+
+// print_r($_POST['apen']);
+
+foreach($_POST['apen'] as $aap) {
+    echo "<img src='apen/".$aap.".jpg'>";
+};
